549d51cbe5e5044a939c55c0e9cfb31773c2e5ac
alexsenga <alexxngabo@gmail.com>
2022-08-03 19:57:54 +0200

    style: profile screen

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -1,98 +1,76 @@
 import { Image, ScrollView, Text, TextInput, View } from 'react-native'
 import React from 'react'
 import styles from './style'
-import Icon from 'react-native-vector-icons/Feather'
+import Feather from 'react-native-vector-icons/Feather'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import Entypo from 'react-native-vector-icons/Entypo'
+import AntDesign from 'react-native-vector-icons/AntDesign'
 
 const Profile = () => {
   return (
     <ScrollView>
       <View style={styles.container}>
-        <View>
+        <View style={styles.topSection}>
           <View>
             <Image source={require('../../images/Alex.jpg')} style={styles.imagesProfile} />
           </View>
-          <View>
-            <Text>Alexis Senga Ngabo</Text>
-            <Text>Senior Full Stack Developer</Text>
+          <View style={styles.names}>
+            <Text style={styles.name}>Alexis Senga Ngabo</Text>
+            <Text style={styles.role}>Senior Full Stack Developer</Text>
+          </View>
+          <View style={styles.iconContainer}>
+            <FontAwesome name='pencil-square-o' size={30} color={'#b6b2b2'} />
           </View>
         </View>
-        <View>
+        <View style={styles.statusContainer}>
           <View>
-            <Text>My Status</Text>
+            <Text style={styles.statusTitle}>My Status</Text>
           </View>
-          <View>
-            <View>
-              <Text>Away</Text>
+          <View style={styles.statusFlex}>
+            <View style={[styles.statusBG, { backgroundColor: '#03035a' }]}>
+              <Text style={styles.statusText}>🤨 Away</Text>
             </View>
-            <View>
-              <Text>At Work</Text>
+            <View style={[styles.statusBG, { backgroundColor: 'lightblue' }]}>
+              <Text style={styles.statusText}>💻 At Work</Text>
             </View>
-            <View>
-              <Text>Gaming</Text>
-            </View>
-          </View>
-        </View>
-        <View>
-          <View>
-            <Text>
-              <Text>Parameter</Text>
-            </Text>
-          </View>
-          <View>
-            <View>
-              <Text>Icon</Text>
-            </View>
-            <View>
-              <Text>Payments</Text>
-            </View>
-            <View>
-              <Text>2 News</Text>
-            </View>
-          </View>
-          <View>
-            <View>
-              <Text>Icon</Text>
-            </View>
-            <View>
-              <Text>Achievements</Text>
-            </View>
-            <View>
-              <Text>Icon</Text>
-            </View>
-          </View>
-          <View>
-            <View>
-              <Text>Icon</Text>
-            </View>
-            <View>
-              <Text>Privacy</Text>
-            </View>
-            <View>
-              <Text>Actions Nested</Text>
+            <View style={[styles.statusBG, { backgroundColor: '#ffa500a8' }]}>
+              <Text style={styles.statusText}>🎮 Gaming</Text>
             </View>
           </View>
         </View>
-        <View>
+
+        <View style={{ marginTop: 10, paddingHorizontal: 5 }}>
           <View>
-            <Text>Email Adress</Text>
-            <TextInput />
+            <Text>My Accout</Text>
           </View>
-          <View>
-            <Text>Username</Text>
-            <TextInput />
+          <View style={styles.inputGroup}>
+            <View>
+              <Entypo name='email' size={18} />
+            </View>
+            <TextInput style={styles.input} placeholder="Email Adress" />
           </View>
-          <View>
-            <Text>Password</Text>
-            <TextInput />
+          <View style={styles.inputGroup}>
+            <View>
+              <AntDesign name='user' size={18} />
+            </View>
+            <TextInput style={styles.input} placeholder="Username" />
           </View>
-          <View>
-            <Text>Birth Date(Optional)</Text>
-            <TextInput />
+          <View style={styles.inputGroup}>
+            <View>
+              <FontAwesome name='lock' size={18} />
+            </View>
+            <View>
+              <FontAwesome name='eye' size={16} />
+            </View>
+            <TextInput style={styles.input} secureTextEntry={true} placeholder="Password" />
+          </View>
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}>Birth Date(Optional)</Text>
+            <TextInput style={styles.input} />
           </View>
         </View>
-        <View>
-          <Text>joined</Text>
-          <Text>21 Jan 2020</Text>
+        <View style={styles.Joined}>
+          <Text><Text style={{ color: '#b6b2b2' }}>joined</Text> 21 Jan 2020</Text>
         </View>
       </View>
     </ScrollView>
--- src/screens/profile/style.js
+++ src/screens/profile/style.js
@@ -9,6 +9,66 @@ const styles = StyleSheet.create({
         width: 80,
         height: 80,
         borderRadius: 50
+    },
+    topSection: {
+        flexDirection: 'row'
+    },
+    names: {
+        marginTop: 10,
+        marginLeft: 26
+    },
+    iconContainer: {
+        position: 'absolute',
+        right: 10,
+        top: 18
+    },
+    name: {
+        fontSize: 22,
+        fontWeight: '500',
+        color: '#000'
+    },
+    role: {
+        fontSize: 14,
+        color: '#b6b2b2',
+        fontWeight: '500',
+        marginTop: 8
+    },
+    statusContainer: {
+        marginTop: 35
+    },
+    statusTitle: {
+        color: '#b6b2b2',
+        fontSize: 14
+    },
+    statusBG: {
+        paddingVertical: 13,
+        paddingHorizontal: 16,
+        borderRadius: 40,
+        marginRight: 20,
+        marginLeft: 3
+    },
+    statusFlex: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 15,
+        marginBottom: 18
+    },
+    statusText: {
+        color: '#e4dfdf',
+        fontSize: 18,
+        fontWeight: '400'
+    },
+    inputGroup: {
+        marginTop: 20
+    },
+    input: {
+        borderBottomColor: 'lightgray',
+        borderBottomWidth: 1,
+        paddingLeft: 30
+    },
+    Joined: {
+        marginTop: 50,
+        marginBottom: 7
     }
 })
 
